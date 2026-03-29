7657eaa93d611749df295fdf36ee5468ebbacd58
Akmal Arzhang <akmalarzhang@gmail.com>
2026-03-29 11:24:33 -0700

    updating cors

--- api/app/middlewares/cors.py
+++ api/app/middlewares/cors.py
@@ -6,7 +6,7 @@ def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.allowed_origins,
-        allow_methods=["*"],
+        allow_methods=["GET", "POST", "PUT", "DELETE"],
         allow_headers=["*"],
         allow_credentials=True,
     )
